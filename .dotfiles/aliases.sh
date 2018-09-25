6ae8da410e5611f49dced81b906903b941f8572a
Will Ockmore <will.ockmore@axomic.com>
2018-09-25 10:05:46 +0100

    add cwl alias

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -1,10 +1,19 @@
 # Aliases
 
+# Python
 alias enva="source env/bin/activate"
 
+
+# Javascript
 alias ni="npm install"
 alias ns="npm start"
 alias nt="npm test"
 
 alias nu="nvm use"
 alias nvmc="nvm current"
+
+
+# AWS
+
+# Fuzzy find CloudWatch Logs and tail them
+alias cwl="saw watch $(saw groups | fzf)"
