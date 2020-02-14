3baf74575fc13748916fb45607d348e3a605cb6c
Will Ockmore <will.ockmore@axomic.com>
2020-02-14 16:02:19 -0500

    add subscription filter command

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -12,3 +12,7 @@ alias nt="npm test"
 
 alias nu="nvm use"
 alias nvmc="nvm current"
+
+
+# Useful AWS commands / aliases
+alias subscription_filters="saw groups | xargs -P8 -I {} aws logs describe-subscription-filters --log-group-name {} | jq -r '.subscriptionFilters | select(length > 0) | .[0] | .logGroupName'"
