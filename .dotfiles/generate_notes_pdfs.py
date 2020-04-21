c21a12739baf79817ab73e05367e7baeb67027f7
Will Ockmore <will.ockmore@axomic.com>
2020-04-21 16:15:51 -0600

    check notes modified time

--- .dotfiles/generate_notes_pdfs.py
+++ .dotfiles/generate_notes_pdfs.py
@@ -10,16 +10,23 @@ def missing_outputs():
     """
     Find and generate missing PDF outputs
     """
+
+    # filename is a date string for input and output
     current_notes = {
-        os.path.splitext(filepath)[0]: os.path.join(NOTES_DIR, filepath)
+        os.path.splitext(filepath)[0]: {
+            "path": os.path.join(NOTES_DIR, filepath),
+            "stat_result": os.stat(os.path.join(NOTES_DIR, filepath)),
+        }
         for filepath in os.listdir(NOTES_DIR)
         if filepath.endswith(".md")
     }
-    generated_pdfs = set(
-        os.path.splitext(filepath)[0]
+    generated_pdfs = {
+        os.path.splitext(filepath)[0]: {
+            "stat_result": os.stat(os.path.join(OUTPUT_DIR, filepath))
+        }
         for filepath in os.listdir(OUTPUT_DIR)
         if filepath.endswith(".pdf")
-    )
+    }
 
     # Generate new pdfs
     pandoc_processes = [
@@ -29,14 +36,16 @@ def missing_outputs():
                 "--variable",
                 "urlcolor=cyan",
                 "--pdf-engine=xelatex",
-                note_filepath,
+                note_meta["path"],
                 "-s",
                 "-o",
-                f"{OUTPUT_DIR}/{note}.pdf",
+                f"{OUTPUT_DIR}/{note_datestr}.pdf",
             ]
         )
-        for note, note_filepath in current_notes.items()
-        if note not in generated_pdfs
+        for note_datestr, note_meta in current_notes.items()
+        if note_datestr not in generated_pdfs
+        or note_meta["stat_result"].st_mtime
+        > generated_pdfs[note_datestr]["stat_result"].st_mtime
     ]
 
     # Wait for processes to finish
