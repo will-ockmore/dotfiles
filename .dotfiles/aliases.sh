fdfe597f93c4a454ed5d4f8a66f9f6137375fcd3
Will Ockmore <will.ockmore@gmail.com>
2020-05-09 13:41:03 -0600

    add linux-specific notes command

--- .dotfiles/aliases.sh
+++ .dotfiles/aliases.sh
@@ -17,13 +17,15 @@ alias nvmc="nvm current"
 # Useful AWS commands / aliases
 alias subscription_filters="saw groups | xargs -P8 -I {} aws logs describe-subscription-filters --log-group-name {} | jq -r '.subscriptionFilters | select(length > 0) | .[0] | .logGroupName'"
 
-# Workflow
-alias notes="python ~/.dotfiles/generate_notes_pdfs.py && open -a /Applications/Firefox.app ~/notes/pdf"
-alias ,t="tig status"
-
-# General open command
+# OS specific aliases
 if [[ "$(uname)" == "Darwin" ]]; then
     alias ']'='open'
+    alias notes="python ~/.dotfiles/generate_notes_pdfs.py && ] -a /Applications/Firefox.app ~/notes/pdf"
 elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
     alias ']'='xdg-open'
+    alias notes="python ~/.dotfiles/generate_notes_pdfs.py && firefox ~/notes/pdf"
 fi
+
+# Workflow
+alias ,t="tig status"
+
