9cae39ec9f55b5917eaf1e126c53e5eb68b6a798
Will Ockmore <will.ockmore@gmail.com>
2022-08-31 15:30:04 +0100

    change keymaps for forward/back word in macos

--- .dotfiles/keymaps.sh
+++ .dotfiles/keymaps.sh
@@ -1,6 +1,6 @@
 # OS-specific keymaps
 if [[ "$(uname)" == "Darwin" ]]; then
-    bindkey "^[b" backward-word     # ⌥←
-    bindkey "^[f" forward-word      # ⌥→
+    bindkey "^[[1;3D" backward-word     # ⌥←
+    bindkey "^[[1;3C" forward-word      # ⌥→
 # elif [[ "$(expr substr $(uname -s) 1 5)" == "Linux" ]]; then
 fi
